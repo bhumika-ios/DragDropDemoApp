8b25a3b7ec2b6de38670b84bb657ad79895eeb2b
bhumika-ios <bhumika.iosdeveloper.com>
2024-08-27 12:51:29 +0530

    Initial Commit

---
+++ DragDropDemoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DragDropDemoApp
+//
+//  Created by Bhumika Patel on 27/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DragDropDemoApp/DragDropDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragDropDemoAppApp.swift
+//  DragDropDemoApp
+//
+//  Created by Bhumika Patel on 27/08/24.
+//
+
+import SwiftUI
+
+@main
+struct DragDropDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
